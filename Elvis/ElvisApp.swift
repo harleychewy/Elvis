9617e75afde31e7b13b2efba31e99d60e4e59ec0
harleychewy <lopeza4869@gmail.com>
2025-02-04 10:56:13 -0800

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Student1 on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
